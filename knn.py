58dd20c9f81968e820c480df9e6dcc0714804f40
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-06-02 23:16:23 +0200

    change confusion matrix call

--- knn.py
+++ knn.py
@@ -1,45 +1,8 @@
-import pandas as pd
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 
+from format_data.process_data import divide_training_test_data
 from plot.plot import plot_with_text
-
-
-def split_dataframe_by_outcome(input_file, output_file_1, output_file_2):
-    df = pd.read_csv(input_file)
-
-    df_dead_discharge = df[df['outcome'].isin([2, 0])]
-    df_hospitalized = df[df['outcome'] == 1]
-
-    df_dead_discharge.to_csv(output_file_1, index=False)
-    df_hospitalized.to_csv(output_file_2, index=False)
-
-
-def suppress_values(input_file, column):
-    df = pd.read_csv(input_file)
-
-    data = df.drop(column, axis=1)
-    labels = df[column]
-    return data, labels
-
-
-def divide_into_training_and_test_data(input_file, column, subset=None):
-    data, labels = suppress_values(input_file, column)
-    if subset is not None:
-        data = data[subset]
-    data_train, data_test, label_train, label_test = train_test_split(data, labels, test_size=0.3, random_state=4)
-
-    imputer = SimpleImputer(strategy='most_frequent')
-    data_train = imputer.fit_transform(data_train)
-    data_test = imputer.transform(data_test)
-
-    scaler = StandardScaler()
-    data_train = scaler.fit_transform(data_train)
-    data_test = scaler.transform(data_test)
-    return data_train, data_test, label_train, label_test
 
 
 def knn_create_model(data_train, label_train, k):
@@ -54,17 +17,13 @@ def accuracy_model(predicted_values, actual_values):
 
 
 def choose_best_k(input_file, save_file):
-    data_train, data_test, outcomes_train, outcomes_test = divide_into_training_and_test_data(input_file, 'outcomes')
-
+    data_train, data_test, outcomes_train, outcomes_test = divide_training_test_data(input_file, 'outcome')
+    x = range(5, 31)
     accuracies = []
     # Iterate through different value of k
-    for k in range(1, 51):
+    for k in x:
         knn_model = knn_create_model(data_train, outcomes_train, k)
-
-        # KNN prediction
         outcomes_pred = knn_model.predict(data_test)
-
-        # Error percentage
         accuracies.append(accuracy_model(outcomes_pred, outcomes_test))
         print(k)
 
@@ -74,14 +33,13 @@ def choose_best_k(input_file, save_file):
     outcomes_pred = knn_model.predict(data_test)
 
     # Plot the results
-    x = range(1, 51)
-    title = 'Error Percentage for K in range(1, 51)'
+    title = 'Accuracy percentage for K in range(1, 31)'
     x_label = 'K'
     y_label = 'Accuracy Percentage'
     plot_with_text(x, accuracies, title, x_label, y_label, best_k, max_accuracy, save_file)
 
-    print(f'Accuracy: {max_accuracy:.2f}' + "%")
-    confusion_matrix(outcomes_pred, outcomes_test)
+    print(f'Max accuracy: {max_accuracy:.2f}' + "%")
+    confusion(outcomes_pred, outcomes_test)
     return best_k, knn_model
 
 
