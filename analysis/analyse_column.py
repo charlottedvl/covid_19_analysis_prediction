a13f39e182993479b2e11752237908a6af4bf93d
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-06-03 09:16:53 +0200

    Create different files

---
+++ analysis/analyse_column.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def get_distinct_values(input_file, column_name):
+    df = pd.read_csv(input_file)
+
+    # Get distinct values and their counts in the specified column
+    value_counts = df[column_name].dropna().value_counts()
+
+    # Print distinct values and their counts
+    print(f"Distinct values in column '{column_name}':")
+    for value, count in value_counts.items():
+        print(f"{value}: {count} occurrences")
+
+
+def variance_column(input_file, column):
+    df = pd.read_csv(input_file)
+    variance = df[column].var()
+    return print(f'The variance of the column {column} is {variance}')
+
+
+def percentage_value_column(input_file, column):
+    df = pd.read_csv(input_file)
+    value_counts = df[column].value_counts(normalize=True) * 100
+    return print(value_counts)
--- analysis/analyse_dataset.py
+++ analysis/analyse_dataset.py
@@ -4,20 +4,7 @@ from sklearn.decomposition import PCA
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
-
 import pandas as pd
-
-
-def get_distinct_values(input_file, column_name):
-    df = pd.read_csv(input_file)
-
-    # Get distinct values and their counts in the specified column
-    value_counts = df[column_name].dropna().value_counts()
-
-    # Print distinct values and their counts
-    print(f"Distinct values in column '{column_name}':")
-    for value, count in value_counts.items():
-        print(f"{value}: {count} occurrences")
 
 
 def analyze_dataset(csv_file):
@@ -106,15 +93,3 @@ def plot_pca_projection(pca_df, save_file=None):
         print(f'Plot saved as {save_file}')
     else:
         plt.show()
-
-
-def variance_column(input_file, column):
-    df = pd.read_csv(input_file)
-    variance = df[column].var()
-    return print(f'The variance of the column {column} is {variance}')
-
-
-def percentage_value_column(input_file, column):
-    df = pd.read_csv(input_file)
-    value_counts = df[column].value_counts(normalize=True) * 100
-    return print(value_counts)
