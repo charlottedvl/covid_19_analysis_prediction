da792e8203eebf1a9315d0dffc7ed916046ccc0b
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-06-16 19:16:28 +0200

    Remove unused function from bayesian_network.py

--- models/bayesian_network.py
+++ models/bayesian_network.py
@@ -4,23 +4,6 @@ from pgmpy.inference import VariableElimination
 from pgmpy.models import BayesianNetwork
 
 
-def conditional_probability(input_file, condition_columns, condition_values, target_column, target_value=None):
-    df = pd.read_csv(input_file)
-    observed_total = 0
-    total = 0
-
-    for index, value in df[target_column].items():
-        if all(pd.notnull(df.at[index, column]) and (condition_value is None or df.at[index, column] == condition_value)
-               for column, condition_value in zip(condition_columns, condition_values)):
-            total += 1
-            if pd.notnull(value) and (target_value is None or value == target_value):
-                observed_total += 1
-
-    if total == 0:
-        return 0
-    return (observed_total / total) * 100
-
-
 def create_bayesian_network(dataset_file):
     df = pd.read_csv(dataset_file)
     model = BayesianNetwork([('visited_Wuhan', 'have_symptoms'),
